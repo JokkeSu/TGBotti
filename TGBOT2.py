3c533bf54095f0f7a55cdf30dd0d205f8412b5b9
JokkeSu <43187863+JokkeSu@users.noreply.github.com>
2018-09-13 02:33:03 +0300

    Update TGBOT2.py

--- TGBOT2.py
+++ TGBOT2.py
@@ -53,9 +53,9 @@ def main():
 
         last_update = greet_bot.get_last_update()
 
-        last_update_id = int(last_update['update_id'])
+        int last_update_id = last_update['update_id']
         last_chat_text = last_update['message']['text']
-        last_chat_id = int(last_update['message']['chat']['id'])
+        int last_chat_id = last_update['message']['chat']['id']
         last_chat_name = last_update['message']['chat']['first_name']
 
         if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
