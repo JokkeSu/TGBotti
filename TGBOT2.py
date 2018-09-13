5dc69730762c26e45b9b1654f51214aca41156ae
JokkeSu <43187863+JokkeSu@users.noreply.github.com>
2018-09-13 09:58:42 +0300

    Update TGBOT2.py

--- TGBOT2.py
+++ TGBOT2.py
@@ -28,7 +28,7 @@ class BotHandler:
         if len(get_result) > 0:
             last_update = get_result[-1]
         else:
-            last_update = get_result[0]
+            last_update = get_result
 
         return last_update
 
