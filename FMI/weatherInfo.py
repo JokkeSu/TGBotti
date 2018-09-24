7dd6c2d3b098557b6005f910d360e4675c331627
ManMadeMachine <Eetu.Suonpaa@hotmail.com>
2018-09-24 23:16:35 +0300

    Added gitignore and todos

--- FMI/weatherInfo.py
+++ FMI/weatherInfo.py
@@ -4,5 +4,26 @@ except ImportError:
     requests = None
     print("Could not import module 'requests'")
 
-class WeatherInfo:
-    api_url = ""
+class WeatherInfo():
+    #TODO: Get api-key from heroku config
+    api_url = "http://data.fmi.fi/fmi-apikey/{}/wfs?request=getCapabilities".format(api_key)
+
+    def getInfo(self):
+        print("TODO: getInfo from {}".format(self.api_url))
+        resp = requests.get(self.api_url)
+
+        print(resp.content)
+
+
+
+def main():
+    wi = WeatherInfo()
+
+    wi.getInfo()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
